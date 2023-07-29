1f6a961c386578b998b1f4968a912a30d5c4f7b6
Folahan <seunfola1@gmail.com>
2023-07-29 06:18:13 +0100

    remodify the user interfac

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ start.addEventListener('click', function () {
         startTimer = setInterval(function () {
             timer();
 
-        }, 1000)
+        }, 2000)
     }
     startTiming();
     alert('The Timer has started')
@@ -66,33 +66,3 @@ pause.addEventListener('click', function () {
 
 });
 
-//notification
-// var permission = Notification.permission;
-
-// if (permission === "granted") {
-//     showNotification();
-// } else if (permission === "default") {
-//     requestAndShowPermission();
-// } else {
-//     alert("Use normal alert");
-// }
-// function showNotification() {
-//     if (document.visibilityState === "visible") {
-//         return;
-//     }
-//     var title = "Alarm Notify";
-//     var icon = "https://homepages.cae.wisc.edu/~ece533/images/airplane.png";
-//     var body = "Note, Time is Up";
-//     var notification = new Notification('Title', { body, icon });
-//     notification.onclick = () => {
-//         notification.close();
-//         window.alert.focus();
-//     }
-// }
-// function requestAndShowPermission() {
-//     Notification.requestPermission(function (permission) {
-//         if (permission === "granted") {
-//             showNotification();
-//         }
-//     });
-// }
